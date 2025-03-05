196f620ed857c8c4f98797cd47137097fda67b61
ummekulsoom <ummekulsoom215@gmail.com>
2025-03-06 01:30:26 +0500

    authentication api integrated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,3 @@
-// import React from "react";
-// import Banner from "../components/Banner";
-// import { Feature } from "../components/Feature";
-// import { Review } from "../components/Review";
-// import { BestSeller } from "../components/BestSeller";
-// import { NewDesignCover } from "../components/NewDesignCover";
-// import { Blog } from "../components/Blog";
-// import Sidebar from "../components/Sidebar";
-
-// const Home = () => {
-//   return (
-//     <>
-//       <Banner />
-//       <Feature />
-//       <Review />
-//       <BestSeller />
-//       <NewDesignCover />
-//       <Blog />
-//       <Sidebar />
-//     </>
-//   );
-// };
-
-// export default Home;
 
 
 import React from "react";
